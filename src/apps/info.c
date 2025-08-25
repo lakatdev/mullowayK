6413f2c6c475c210bdd2e965f8d8c43b24f4d3bd
Fodor Róbert <fodor.robert@szikramozgalom.hu>
2025-08-26 01:25:37 +0200

    version

--- src/apps/info.c
+++ src/apps/info.c
@@ -3,7 +3,7 @@
 void app_info_draw()
 {
     draw_screen(THEME_BACKGROUND_COLOR);
-    draw_text(10, 30, "MullowayK build 2025-08-25", 24, THEME_TEXT_COLOR);
+    draw_text(10, 30, "MullowayK 1.0 build 2025-08-26", 24, THEME_TEXT_COLOR);
     draw_text(10, 54, "Using Keszeg 4 interpreter.", 24, THEME_TEXT_COLOR);
 }
 
